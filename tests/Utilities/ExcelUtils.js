5648596c2dfc00e0356e90b2c35b00d65d78a9a5
AmreenNazia <156974279+AmreenNazia@users.noreply.github.com>
2024-11-07 22:39:55 -0600

    Updated ExcelReader

--- tests/Utilities/ExcelUtils.js
+++ tests/Utilities/ExcelUtils.js
@@ -1,22 +1,30 @@
-const ExcelJS = require('exceljs');
+// const ExcelJS = require('exce');
+const xlsx = require('xlsx');
 
-async function readExcelFile(SheetPath, SheetName) {
-   const workbook = new ExcelJS.Workbook();
-   await workbook.xlsx.readFile(SheetPath);
-   const worksheet = workbook.getWorksheet(SheetName);
-
-   const data = [];
-   worksheet.eachRow((row, rowNumber) => {
-      // Assuming your Excel data has headers in the first row and data below
-      if (rowNumber > 1) {  // Skip the header row (if any)
-         data.push({
-            username: row.getCell(1).value,  // Assuming username is in the first column
-            password: row.getCell(2).value   // Assuming password is in the second column
-         });
-      }
-   });
-
-   return data;  // Return the data array
+function readExcelFile(filepath, sheetName){
+   const workbook = xlsx.readFile(filepath);
+   const sheet = workbook.Sheets[sheetName];
+   const jsonData = xlsx.utils.sheet_to_json(sheet);
+   return jsonData;
 }
+
+// async function readExcelFile(SheetPath, SheetName) {
+//    const workbook = new ExcelJS.Workbook();
+//    await workbook.xlsx.readFile(SheetPath);
+//    const worksheet = workbook.getWorksheet(SheetName);
+
+//    const data = [];
+//    worksheet.eachRow((row, rowNumber) => {
+//       // Assuming your Excel data has headers in the first row and data below
+//       if (rowNumber > 1) {  // Skip the header row (if any)
+//          data.push({
+//             username: row.getCell(1).value,  // Assuming username is in the first column
+//             password: row.getCell(2).value   // Assuming password is in the second column
+//          });
+//       }
+//    });
+
+//    return data;  // Return the data array
+// }
 
 module.exports = { readExcelFile };
