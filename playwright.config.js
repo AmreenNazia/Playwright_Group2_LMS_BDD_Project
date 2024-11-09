75c9b866a4643ffe680db3b49a2c372803c590b6
AmreenNazia <156974279+AmreenNazia@users.noreply.github.com>
2024-11-08 18:36:50 -0600

    Updated config

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
  
-//  retries: 1,
+  retries: 1,
  
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
@@ -86,4 +86,5 @@ module.exports = defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  
 });
